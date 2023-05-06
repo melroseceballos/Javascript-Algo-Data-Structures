864c7698a427f99085e11e8ecb465d470aa0d54e
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-06 11:23:54 -0700

    added best case solution Frequency Counters

--- Patterns/FrequencyCounters.js
+++ Patterns/FrequencyCounters.js
@@ -9,6 +9,49 @@
  */
 
 
+/**** BEST CASE SOLUTION O(n) */
+// first, the solution checks if the length of both arrays are the same
+// if it is, it sends it to their respective for loops
+// if not, the solution sends false - end of solution
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    // these variables are initialized because the end output of solution would be an object
+    // and also to store the iterated numbers in the array inside an object
+    // because objects are faster to access (linear)
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    // in here it checks if there is a in the frequency counter object
+    // if there is - it adds 1 to the value 
+    // if not it creates another key
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+    
+    // checks if there is a squared value in array1
+    // if there is none then the solution returns false
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        // if it returns true, then it checks how many times those numbers are in the array2
+        // if is the same number of times in array1 then it returns true
+        // if not the same it returns false
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    return true
+}
+
+same([1,2,3,2,5], [9,1,4,4,11])
 
 
 
@@ -16,15 +59,7 @@
 
 
 
-
-
-
-
-
-
-
-
-/**** NAIVE SOLUTION */
+/**** WORST CASE SOLUTION 0(n) ^ 2 */
 // the function takes 2 arrays [arr1] [arr2]
 function same(arr1, arr2){
     // it checks if the length of both array are the same, if not, it returns false
