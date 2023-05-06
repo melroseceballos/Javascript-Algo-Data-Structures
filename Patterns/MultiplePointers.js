d4f08dee55e135a87d1a87c077db69eb67be257b
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-06 14:42:48 -0700

    added worst and best case for MultiplePointers

--- Patterns/MultiplePointers.js
+++ Patterns/MultiplePointers.js
@@ -4,3 +4,21 @@
  * if the a pair is found, it should return an array containing the pair
  * if pair does not exist, it should return undefined
 */
+
+
+
+// BEST CASE SOLUTION: Time Complexity O(n) Space Complexity O(1)
+function sumZero(arr) {
+    let left = 0
+    let right = 0
+    while (left <right ){
+        let sum  = arr[left] + arr[right]
+        if (sum === 0){
+            return arr[left], arr[right]
+        }else if (sum > 0){
+            right --
+        }else {
+            left++
+        }
+    }
+}
