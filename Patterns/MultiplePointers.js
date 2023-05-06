79387a9a3208b1166531bb9d3df7989c19c0606c
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-06 14:30:55 -0700

    adde problem to multiple pointers

---
+++ Patterns/MultiplePointers.js
@@ -0,0 +1,6 @@
+/*** PROBLEM 
+ * write a function that accepts a sorted array of integers
+ * the function should the first pair that when added to together equals zero
+ * if the a pair is found, it should return an array containing the pair
+ * if pair does not exist, it should return undefined
+*/
