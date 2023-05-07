cf4ca90e9b1527380d5be81b7739f08aad4753de
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-07 12:01:54 -0700

    added problem - divide and conquer

---
+++ Patterns/DivideAndConquer.js
@@ -0,0 +1,4 @@
+/***** PROBLEM
+ * Given a sorted array, write a function called search that accepts a value and returns the index where the value is located,
+ * if the value is non-existent in the array .. return -1
+ */
