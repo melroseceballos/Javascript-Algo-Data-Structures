a1306663d234c5e621a009c5101965c0446fa67c
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-07 10:52:49 -0700

    added notes to pointers

---
+++ Patterns/SlidingWindow.js
@@ -0,0 +1,14 @@
+/***** PROBLEM
+ * write a function that accepts an array of integers and a number called n. The function should calculate the maximum sum of n.
+ * basically what the function is asking for is that the you're going to be given an array
+ * and then there will be a number that tells you how many numbers you can access in order to get the maximum sum
+ * so basically: ([1,2,3,4,5,6,7], 2) <----- so the 2 in there is telling you -- you can use two numbers in the array in order to give biggest sum 
+ * obviously it would be 6 & 7 = 13
+ * so what the sliding window does is that it checks 1&2 and sets the temporary maximum number to 3
+ * and then it checks 2 & 3 .. is it bigger than the maximum sum than previously? if yes, then it replaces the temporary max number
+ * and so on so forth
+ * now.. the window can change in size depending on the subset you were given.. in the example we have 2. 
+ * that can change to any number like 4 ... now you have to then find the largest sum using 4 numbers in the array
+ * NOTE: THEY HAVE TO BE IN ORDER -- YOU CAN'T SKIP A NUMBER or start from different side.. those are pointers.. 
+ * you have to start from beginning to end
+ */
