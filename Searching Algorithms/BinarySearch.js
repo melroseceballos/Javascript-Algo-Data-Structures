11ccad448ea056ba8ce16bed1f04efe8a527b33f
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-08 13:21:39 -0700

    added binarySearch

--- Searching Algorithms/BinarySearch.js
+++ Searching Algorithms/BinarySearch.js
@@ -34,3 +34,17 @@ function binarySearch(arr,val){
     // if target value is not found .. -1 is returned
        return -1
 }
+
+// string search example
+function naiveSearch(long, short){
+    var count = 0;
+    for(var i = 0; i < long.length; i++){
+        for(var j = 0; j < short.length; j++){
+           if(short[j] !== long[i+j]) break;
+           if(j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
+
+naiveSearch("lorie loled", "lol")
