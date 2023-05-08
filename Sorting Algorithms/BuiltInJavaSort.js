cd86f842b529c4fab4d53590952335a583204174
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-08 14:19:20 -0700

    added samples in BuiltInJavaSorts

--- Sorting Algorithms/BuiltInJavaSort.js
+++ Sorting Algorithms/BuiltInJavaSort.js
@@ -1,6 +1,20 @@
+/*Ex: using .sort()
+- sorts array numerically
+**/
+
 function javaSort (num1, num2){
+    // num1-num2 this sorts array from smallest to biggest
+
     return num1 - num2
     }
-    
+            // using the .sort here
     console.log([1,2,4,9,7,3,5,8,6].sort(javaSort))
     
+    function javaSort (num1, num2){
+        // this now sorts from biggest to smallest
+        
+        return num2 - num1
+        }
+            // using the .sort here
+        console.log([1,2,4,9,7,3,5,8,6].sort(javaSort))
+        
