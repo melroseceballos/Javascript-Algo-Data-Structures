d9204990926a86b8449aef94ad8f415bb5cb2fce
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 09:26:00 -0700

    added notes to mergesort solution

--- Sorting Algorithms/MergeSort.js
+++ Sorting Algorithms/MergeSort.js
@@ -25,11 +25,21 @@ function merge(arr1, arr2){
 
 // Recrusive Merge Sort
 function mergeSort(arr){
+    // checks if the array contains elements in the array. Basically if it is not empty
     if(arr.length <= 1) return arr;
+    // located the middle of the array 
+    // and then passes that index/number into the middle variable
     let mid = Math.floor(arr.length/2);
+    // splits up the left side of the aray from the right side of the array
+    // so now there's two arrays instead of one
     let left = mergeSort(arr.slice(0,mid));
+    // splits up the right side of the array from the left side
+    // we start from mid because if we start at 0 == that is left zone
+    // so we start from the middle
     let right = mergeSort(arr.slice(mid));
-    return merge(left, sright);
+    // after everything is sorted -- we merged them back together 
+    // now they are sorted!
+    return merge(left, right);
 }
 
 mergeSort([10,24,76,73])
