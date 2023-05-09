a0a15bf265f6047a8768ffc0cad405081ef72039
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 14:22:07 -0700

    added node samples

--- Data Structures/SinglyLinkedLists.js
+++ Data Structures/SinglyLinkedLists.js
@@ -2,3 +2,17 @@
  * contains data/value
  * points to the next node
  */
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+let first = new Node ('Hi')
+first.next = new Node ('How are you?')
+first.next.next = new Node ('How do you like the weather?')
+first.next.next.next = new Node ('It\'s pretty hot here!')
+
+console.log(first)
