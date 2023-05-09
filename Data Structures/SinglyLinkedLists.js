b6dd918e6842877d539001a60195ea13f7dc8f78
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 16:08:52 -0700

    added set method

--- Data Structures/SinglyLinkedLists.js
+++ Data Structures/SinglyLinkedLists.js
@@ -97,3 +97,25 @@ shift() {
     this.length++;
     return this;
 }
+
+// GET
+get(index){
+    if(index < 0 || index >= this.length) return null;
+    var counter = 0;
+    var current = this.head;
+    while(counter !== index){
+        current = current.next;
+        counter++;
+    }
+    return current;
+}
+
+// SET
+set(index, val){
+    var foundNode = this.get(index);
+    if(foundNode){
+        foundNode.val = val;
+        return true;
+    }
+    return false;
+}
