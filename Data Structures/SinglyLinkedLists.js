86fbbb279a5e723f2742d7caca30b98462a7904d
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 15:39:10 -0700

    added shift solution

--- Data Structures/SinglyLinkedLists.js
+++ Data Structures/SinglyLinkedLists.js
@@ -85,4 +85,15 @@ shift() {
   }
 
   // UNSHIFT
-  
+  unshift(val){
+    var newNode = new Node(val);
+    if(!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+    } else {
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+    this.length++;
+    return this;
+}
