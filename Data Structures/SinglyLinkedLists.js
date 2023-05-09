6b275e29588d22364071a7095fa9c8c19c505ffd
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 16:26:27 -0700

    added insert solution

--- Data Structures/SinglyLinkedLists.js
+++ Data Structures/SinglyLinkedLists.js
@@ -121,3 +121,16 @@ set(index, val){
 }
 
 // INSERT
+insert(index, val){
+    if(index < 0 || index > this.length) return false;
+    if(index === this.length) return !!this.push(val);
+    if(index === 0) return !!this.unshift(val);
+    
+    var newNode = new Node(val);
+    var prev = this.get(index - 1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+}
