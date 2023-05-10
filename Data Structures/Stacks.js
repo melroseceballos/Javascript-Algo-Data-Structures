72c6d00b54932292ed92f0546e9e6ec69a50ae9b
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 22:15:48 -0700

    added solution to stacl

--- Data Structures/Stacks.js
+++ Data Structures/Stacks.js
@@ -1,2 +1,20 @@
 // LIFO IN LINKEDLIST IS PUSH/POP when using stacks
 
+// push
+/***** DO LINKEDLIST SKELETON HERE
+ * WHICH INCLUDES CREATING A NODE CLASS
+ * AND A SINGLYLINKEDLIST CLASS
+ */
+push(val)   {
+    let newNode = new Node(val)
+    if(this.length === 0){
+        this.first = newNode
+        this.last = newNode
+    }else(this.length === 1){
+        let firstProp = this.first
+         this.first = newNode
+        this.first.next = firstProp
+    }
+    return ++this.size
+
+}
