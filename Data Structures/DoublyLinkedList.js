4af717a5950b34a454c861d2a38cc6d4a97a241e
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 21:01:45 -0700

    get method done DLL

--- Data Structures/DoublyLinkedList.js
+++ Data Structures/DoublyLinkedList.js
@@ -54,6 +54,28 @@ class DoublyLinkedList {
         this.length--;
         return oldHead;
     }
+    // GET
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+
 }
 
 var list = new DoublyLinkedList()
