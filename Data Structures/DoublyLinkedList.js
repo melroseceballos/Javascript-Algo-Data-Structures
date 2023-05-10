14db02ceea32b9bce2b2c78b13a3e5103369edca
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-09 19:16:35 -0700

    added push method

--- Data Structures/DoublyLinkedList.js
+++ Data Structures/DoublyLinkedList.js
@@ -6,10 +6,25 @@ class Node{
     }
 }
 
-class DoublyLinkedList{
+
+class DoublyLinkedList {
     constructor(){
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+        
