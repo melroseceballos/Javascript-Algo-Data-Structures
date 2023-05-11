e7f6f3138ab0b4e2a12a6d44b5dc643ed40ea26f
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-11 09:56:50 -0700

    added comments to linkedlistcycle

--- Leetcode/LinkedListCycle.js
+++ Leetcode/LinkedListCycle.js
@@ -11,15 +11,24 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    // create a variable for slow pointer
     let slow = head
+    // creating a variable for fast points
     let fast = head
+    // make sure that there is a value for the fast and fast.next
     while(fast && fast.next !== null){
+        // incrementing slow pointer by 1
         slow = slow.next
+        // incrementing fast point by 2
         fast = fast.next.next
+        // if they catch up to each other that means that there is a 
+        // cycle going on
         if(slow === fast){
             return true
         }
     }
+    // if they don't catch up to each other
+    // that means there's no cycle 
     return false
 };
 
