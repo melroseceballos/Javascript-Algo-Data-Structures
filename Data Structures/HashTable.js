db43cc5dfbff6544f6e7933aecaa0cf42fe123be
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-13 15:21:17 -0700

    added values method

--- Data Structures/HashTable.js
+++ Data Structures/HashTable.js
@@ -110,3 +110,17 @@ keys(){
   }
   return keysArr;
 }
+
+values(){
+  let valuesArr = [];
+  for(let i = 0; i < this.keyMap.length; i++){
+    if(this.keyMap[i]){
+      for(let j = 0; j < this.keyMap[i].length; j++){
+        if(!valuesArr.includes(this.keyMap[i][j][1])){
+          valuesArr.push(this.keyMap[i][j][1])
+        }
+      }
+    }
+  }
+  return valuesArr;
+}
