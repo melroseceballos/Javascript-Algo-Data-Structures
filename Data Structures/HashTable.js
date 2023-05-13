0c6c46894e59e4cb68dcaf479f1b489009ab1d00
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-13 14:52:08 -0700

    added optimized hash function

--- Data Structures/HashTable.js
+++ Data Structures/HashTable.js
@@ -67,3 +67,15 @@ function hash(key, arr){
   }
   return total
 }
+
+// an optimized hash function that uses prime
+function hash (key,arr){
+  let total = 0;
+  let prime = 31;
+  for(let i = 0; i<Math.min(key.length, 100); i++){
+    let char = key[i]
+    let value = char.charCodeAt(0) - 96
+    total = (total * prime + value) % arr
+  }
+  return total;
+}
