e6f905ae20ff4cf10d87d0ee68428386de0602ba
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-15 09:41:30 -0700

    added vertex

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -3,4 +3,9 @@ class Graph {
     constructor() {
         this.adjacencyList = {}
     }
+    addVertex(vertex){
+        // to check if there's already an adjanceny list for vertex
+        
+        this.adjacencyList[vertex] = []
+    }
 }
