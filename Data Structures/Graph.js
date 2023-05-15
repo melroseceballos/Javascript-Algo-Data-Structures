5ad8062db799a9a50be1a3a139ab8b94cc5277fc
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-15 09:54:31 -0700

    added edge

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -9,4 +9,9 @@ class Graph {
         if(!this.adjacencyList[vertex])  this.adjacencyList[vertex] = []
        
     }
+    addEdge(v1, v2){
+          this.adjacencyList[v1].push(v2)
+          this.adjacencyList[v2].push(v1)
+    }
+  
 }
