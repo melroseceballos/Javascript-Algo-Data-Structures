a0b943e841ff0d1df8692d42134c39bb5eff4d4a
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-16 12:59:16 -0700

    added BFS traversal

---
+++ Data Structures/GraphTraversal.js
@@ -0,0 +1,5 @@
+// visits an vertex
+// after a vertex has been visited
+// it is crossed as being visited and will no longer need to visit the same node
+// the loop continues to go through the graph 
+// until all the vertices has been visited
