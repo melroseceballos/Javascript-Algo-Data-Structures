8df51e15362873b8b9864c96120e2f29d4c9fda2
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-16 14:13:00 -0700

    added recursive methods for DFS Graph Traversal

--- Data Structures/GraphTraversal.js
+++ Data Structures/GraphTraversal.js
@@ -1,8 +1,22 @@
 
-/********* DFS */
+/********* DFS 
+visits an vertex
+after a vertex has been visited
+ it is crossed as being visited and will no longer need to visit the same node the loop continues to go through the graph 
+until all the vertices has been visited
+*/
 
-// visits an vertex
-// after a vertex has been visited
-// it is crossed as being visited and will no longer need to visit the same node
-// the loop continues to go through the graph 
-// until all the vertices has been visited
+// RECURSIVE DFS TRAVERSAL
+(function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    
