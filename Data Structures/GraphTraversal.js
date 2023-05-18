799ba17b5cd96307d29b538359c3632af6d08f07
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-18 00:37:41 -0700

    added BFS solution

--- Data Structures/GraphTraversal.js
+++ Data Structures/GraphTraversal.js
@@ -81,3 +81,24 @@ depthFirstRecursive(start) {
 
         //BREADTH FIRST SEARCH
         // uses push and shift instead of push and pop
+        breadthFirst(start){
+            const queue = [start];
+            const result = [];
+            const visited = {};
+            let currentVertex;
+            visited[start] = true;
+    
+            while(queue.length){
+                currentVertex = queue.shift();
+                result.push(currentVertex);
+               
+    
+                this.adjacencyList[currentVertex].forEach(neighbor => {
+                    if(!visited[neighbor]){
+                        visited[neighbor] = true;
+                        queue.push(neighbor);
+                    }
+                });
+            }
+            return result;
+        }
